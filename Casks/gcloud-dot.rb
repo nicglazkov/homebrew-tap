c79ab089e9f75d29e1bcfcecbb0cd4db644912db
Nic Glazkov <82780940+nicglazkov@users.noreply.github.com>
2026-08-09 17:10:15 -0400

    Point the cask back at 1.0.2 until the 1.0.3 disk image is built

--- Casks/gcloud-dot.rb
+++ Casks/gcloud-dot.rb
@@ -1,6 +1,6 @@
 cask "gcloud-dot" do
-  version "1.0.3"
-  sha256 ""
+  version "1.0.2"
+  sha256 "779221539ccb3dc8be03b38c1b08d13f2789ef3d68467222ae67e1373d6738d2"
 
   url "https://github.com/nicglazkov/gcloud-dot/releases/download/v#{version}/GCloud-Dot-#{version}.dmg",
       verified: "github.com/nicglazkov/gcloud-dot/"
