a6a2947ffc4a8e595bd3bba083e0b520654bcb8c
Nic Glazkov <82780940+nicglazkov@users.noreply.github.com>
2026-08-12 01:46:10 -0700

    gcloud-dot 1.0.5

--- Casks/gcloud-dot.rb
+++ Casks/gcloud-dot.rb
@@ -1,6 +1,6 @@
 cask "gcloud-dot" do
-  version "1.0.4"
-  sha256 "428f83be814971d642f0a9091cd1c16223a48b065111043fdabea3c7c09624cc"
+  version "1.0.5"
+  sha256 "a1975fc166c193bab3d371ee17244f1672c6868e31a8e1cb5b53f63ace4716e8"
 
   url "https://github.com/nicglazkov/gcloud-dot/releases/download/v#{version}/GCloud-Dot-#{version}.dmg",
       verified: "github.com/nicglazkov/gcloud-dot/"
